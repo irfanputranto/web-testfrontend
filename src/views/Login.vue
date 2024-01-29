70bf1561eebc6f60d164a8812b3580ac44fcafff
irfanputranto <putrantoirfan@gmail.com>
2024-01-29 11:36:02 +0700

    update

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -14,26 +14,41 @@ const login = async () => {
     try {
         await authStore.login(email.value, password.value);
 
-        router.push({name: 'article'});
+        router.push({ name: 'article' });
     } catch (err) {
-       error.value = 'Invalid email or password';
+        error.value = 'Invalid email or password';
     }
 }
 </script>
 
 <template>
-    <div>
-        <h2>Login</h2>
-        <form @submit.prevent="login()">
-            <label for="email">Email:</label>
-            <input v-model="email" type="email" required>
-            <br />
-            
-            <label for="password">Password:</label>
-            <input v-model="password" type="password" required>
-            <br />
-           <ButtonVue type="submit">Login</ButtonVue>
-            <p v-if="error" class="text-red-800">{{ error }}</p>
-        </form>
+    <div class="container mx-auto mt-4">
+        <section class="min-h-[1rem]">
+        <div class="w-full mx-auto my-96 max-w-xs">
+            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" @submit.prevent="login()">
+                <h2 class="text-center mb-2">Login</h2>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                        Email
+                    </label>
+                    <input v-model="email" type="email"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="email" placeholder="Email@gmail.com" required>
+                </div>
+                <div class="mb-6">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                        Password
+                    </label>
+                    <input v-model="password" type="password" required
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        id="password" placeholder="******************">
+                </div>
+                <div class="flex items-center justify-between">
+                    <ButtonVue type="submit">Login</ButtonVue>
+                </div>
+            </form>
+        </div>
+</section>
+
     </div>
 </template>
